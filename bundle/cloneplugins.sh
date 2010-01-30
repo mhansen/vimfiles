aa769e8664701c9b5e629a27218be559e052003e
Mark Hansen <mark@markhansen.co.nz>
2010-01-30 18:18:54 +1300

    new script to automatically get the latest versions of plugins

---
+++ bundle/cloneplugins.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone http://github.com/scrooloose/nerdtree.git
+git clone http://github.com/msanders/snipmate.vim.git
+git clone http://github.com/tpope/vim-rails.git
+git clone http://repo.or.cz/r/vcscommand.git
